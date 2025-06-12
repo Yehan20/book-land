c1db637cc1bebd88c7e72f88a582df3b0bde3f1e
Yehan20 <yehansk15@gmail.com>
2025-06-13 01:45:30 +0530

    chore(provider): register repository and service bindings in AppServiceProvider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Interfaces\AuthorRepositoryInterface;
+use App\Interfaces\BookRepositoryInterface;
+use App\Repositories\AuthorRepository;
+use App\Repositories\BookRepository;
+use App\Services\AuthorService;
+use App\Services\BookService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Register the  Repository interface  and Repository classes in the Service container
+        $this->app->bind(BookRepositoryInterface::class, BookRepository::class);
+        $this->app->bind(AuthorRepositoryInterface::class, AuthorRepository::class);
+
+        $this->app->bind(BookService::class, function ($app) {
+            return new BookService($app->make(BookRepositoryInterface::class));
+        });
+        $this->app->bind(AuthorService::class, function ($app) {
+            return new AuthorService($app->make(AuthorRepositoryInterface::class));
+        });
+
+        //Register telescope
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
