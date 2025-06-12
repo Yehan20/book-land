62383182db3669e896d749f5714f00d8a8d89628
Yehan20 <yehansk15@gmail.com>
2025-06-13 02:03:13 +0530

    chore(factory): update UserFactory with assigning role after creation of user

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -2,9 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -37,8 +39,18 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    //  Assign role after creation
+    public function configure(): static
+
+    {
+        return $this->afterMaking(function (User $user) {})->afterCreating(function (User $user) {
+            $roleName = Role::where('name', 'customer')->first();
+            $user->assignRole($roleName);
+        });
     }
 }
