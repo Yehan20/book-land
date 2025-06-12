0b82686b010dbf79e28c3533ced388c84316fb39
Yehan20 <yehansk15@gmail.com>
2025-06-13 01:53:40 +0530

    feat(seeder): update DatabaseSeeder to include new seeder classes

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,16 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
+       
+       // Initilize mock up date
         $this->call([
-            //  WriterSeeder::class
-            RoleSeeder::class
+           RoleSeeder::class,
+           AuthorSeeder::class,
+           CustomerSeeder::class,
+           BookSeeder::class,
         ]);
     }
 }
