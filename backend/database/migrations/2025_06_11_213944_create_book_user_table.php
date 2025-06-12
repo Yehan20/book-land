db7f4a42bb23a03618efedea20ac212180c850f0
Yehan20 <yehansk15@gmail.com>
2025-06-12 23:42:07 +0530

    feat(migration): create books, authors, and book_user table

---
+++ backend/database/migrations/2025_06_11_213944_create_book_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->date('date_of_borrow');
+            $table->date('date_of_return')->nullable();
+            $table->date('deadline')->comment('Must return the book on or before the current date');
+            $table->boolean('is_returned')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book_user');
+    }
+};
