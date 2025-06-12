d4e39f29cfd1e198a3693c0c9dfd1df3e19d8383
Yehan20 <yehansk15@gmail.com>
2025-06-13 00:49:03 +0530

    feat(repository-interface): define interfaces for Book and Author repositories with method blueprints

---
+++ backend/app/Interfaces/AuthorRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Models\Author;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+interface AuthorRepositoryInterface
+{
+    //
+    public function all(): LengthAwarePaginator;
+    public function dropdown(): Collection; // just id & title
+    public function create(array $data): Author;
+    public function update(array $data, int $id): Author;
+    public function delete(int $id): bool;
+    public function  find(int $id): Author;  // throws ModelNotFoundException if not found
+    // public function getFulfilledOrders();
+}
---
+++ backend/app/Interfaces/BookRepositoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+interface BookRepositoryInterface
+{
+
+    public function allPaginated(string|null $params): LengthAwarePaginator;
+    public function create(array $data): Book;
+    public function update(array $data, int $id): Book;
+    public function delete(int $id): bool;
+    public function find(int $id): Book;  // throws ModelNotFoundException if not found
+    public function rentBookByUser(User $user,int $bookId,array $data):void;
+    public function returnBookByUser(int $bookId,int $userId,array $data):void;
+    public function getBooksByUser(User $user,string $condition = ''):LengthAwarePaginator;
+
+}
