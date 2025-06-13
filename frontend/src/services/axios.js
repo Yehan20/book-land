ee835f48dd9570920bbb854f73d7e5ebcb64a0c0
Yehan20 <yehansk15@gmail.com>
2025-06-13 23:16:59 +0530

    feat(axios) congigure instance and set up base url

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-// Customization of axios instance to add our base our and allow  send cookei containing jwt
+// Customization of axios instance to add our base our and allow  send cookie containing jwt
 const axiosClient = axios.create({
   baseURL:'http://localhost:8000/api/',
   // import.meta.env.VITE_API_BASE_URL, 
